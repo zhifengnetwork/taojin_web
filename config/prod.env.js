40d7c108f38dd2fc53fa34eafe0fcf6d8f243fcb
webhzp <1225613448@qq.com>
2019-12-24 11:42:54 +0800

    a

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"https://taojin.zhifengwangluo.com/api/"'
+  API_ROOT: '"https://www.519991.cn/api/"'
+  // API_ROOT: '"https://taojin.zhifengwangluo.com/api/"'
 }
